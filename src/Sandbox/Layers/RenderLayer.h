68c8dec449730150ba9cce5b48e3eed6a0777ff5
Sascha Häusler <saeschdivara@gmail.com>
2021-09-25 16:03:27 +0200

    Reformat multiline strings

--- src/Sandbox/Layers/RenderLayer.h
+++ src/Sandbox/Layers/RenderLayer.h
@@ -39,31 +39,31 @@ namespace Sandbox {
             m_BufferContainer->AddBuffer(m_IndexBuffer);
 
             std::string vertexSrc = R"(
-            #version 330 core
+                #version 330 core
 
-            layout(location = 0) in vec3 a_Position;
-            layout(location = 1) in vec4 a_Color;
+                layout(location = 0) in vec3 a_Position;
+                layout(location = 1) in vec4 a_Color;
 
-            out vec4 v_Color;
+                out vec4 v_Color;
 
-            void main()
-            {
-                v_Color = a_Color;
-                gl_Position = vec4(a_Position, 1.0);
-            }
-        )";
+                void main()
+                {
+                    v_Color = a_Color;
+                    gl_Position = vec4(a_Position, 1.0);
+                }
+            )";
 
             std::string fragmentSrc = R"(
-            #version 330 core
-            layout(location = 0) out vec4 color;
+                #version 330 core
+                layout(location = 0) out vec4 color;
 
-            in vec4 v_Color;
+                in vec4 v_Color;
 
-            void main()
-            {
-                color = v_Color;
-            }
-        )";
+                void main()
+                {
+                    color = v_Color;
+                }
+            )";
 
             m_Shader = Shell::CreateScope<Shell::Shader>(vertexSrc, fragmentSrc);
         };
