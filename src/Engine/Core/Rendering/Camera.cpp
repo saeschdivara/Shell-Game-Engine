4586206b7ab1375566be522d2a379218d335bdf2
Sascha Häusler <saeschdivara@gmail.com>
2021-09-25 23:07:00 +0200

    Default camera position is 0, 0, 0

--- src/Engine/Core/Rendering/Camera.cpp
+++ src/Engine/Core/Rendering/Camera.cpp
@@ -6,7 +6,8 @@ namespace Shell {
     OrthographicCamera::OrthographicCamera(float left, float right, float bottom, float top)
         : m_ProjectionMatrix(glm::ortho(left, right, bottom, top, -1.0f, 1.0f)),
           m_ViewMatrix(1.0f),
-          m_ViewProjectionMatrix(m_ProjectionMatrix * m_ViewMatrix)
+          m_ViewProjectionMatrix(m_ProjectionMatrix * m_ViewMatrix),
+          m_Position(0.0f)
     {
     }
 
