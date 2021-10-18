686029be95fb3077ee42e35ec72dd9493bb55806
Sascha Häusler <saeschdivara@gmail.com>
2021-10-18 21:52:31 +0200

    #5 scene save system - Add uuid implementation

---
+++ src/Engine/Utils/Uuid.cpp
@@ -0,0 +1,117 @@
+#include "Uuid.h"
+
+#ifdef SHELL_PLATFORM_WINDOWS
+
+#define UUID_SYSTEM_GENERATOR
+
+    #ifdef UUID_SYSTEM_GENERATOR
+        #include <objbase.h>
+    #endif
+
+    #ifdef UUID_TIME_GENERATOR
+        #include <iphlpapi.h>
+        #pragma comment(lib, "IPHLPAPI.lib")
+    #endif
+
+#elif SHELL_PLATFORM_LINUX
+
+    #ifdef UUID_SYSTEM_GENERATOR
+        #include <uuid/uuid.h>
+    #endif
+
+#elif SHELL_PLATFORM_MACOS
+
+    #ifdef UUID_SYSTEM_GENERATOR
+        #include <CoreFoundation/CFUUID.h>
+    #endif
+
+#endif
+
+namespace Shell {
+    Uuid Uuid::Create() {
+#ifdef SHELL_PLATFORM_WINDOWS
+        GUID newId;
+        ::CoCreateGuid(&newId);
+
+        std::array<uint8_t, 16> bytes =
+                { {
+                          (unsigned char)((newId.Data1 >> 24) & 0xFF),
+                          (unsigned char)((newId.Data1 >> 16) & 0xFF),
+                          (unsigned char)((newId.Data1 >> 8) & 0xFF),
+                          (unsigned char)((newId.Data1) & 0xFF),
+
+                          (unsigned char)((newId.Data2 >> 8) & 0xFF),
+                          (unsigned char)((newId.Data2) & 0xFF),
+
+                          (unsigned char)((newId.Data3 >> 8) & 0xFF),
+                          (unsigned char)((newId.Data3) & 0xFF),
+
+                          newId.Data4[0],
+                          newId.Data4[1],
+                          newId.Data4[2],
+                          newId.Data4[3],
+                          newId.Data4[4],
+                          newId.Data4[5],
+                          newId.Data4[6],
+                          newId.Data4[7]
+                  } };
+
+        return Uuid{ std::begin(bytes), std::end(bytes) };
+
+#elif SHELL_PLATFORM_LINUX
+
+        uuid_t id;
+        uuid_generate(id);
+
+         std::array<uint8_t, 16> bytes =
+         { {
+               id[0],
+               id[1],
+               id[2],
+               id[3],
+               id[4],
+               id[5],
+               id[6],
+               id[7],
+               id[8],
+               id[9],
+               id[10],
+               id[11],
+               id[12],
+               id[13],
+               id[14],
+               id[15]
+            } };
+
+         return uuid{ std::begin(bytes), std::end(bytes) };
+
+#elif SHELL_PLATFORM_MACOS
+         auto newId = CFUUIDCreate(NULL);
+         auto bytes = CFUUIDGetUUIDBytes(newId);
+         CFRelease(newId);
+
+         std::array<uint8_t, 16> arrbytes =
+         { {
+               bytes.byte0,
+               bytes.byte1,
+               bytes.byte2,
+               bytes.byte3,
+               bytes.byte4,
+               bytes.byte5,
+               bytes.byte6,
+               bytes.byte7,
+               bytes.byte8,
+               bytes.byte9,
+               bytes.byte10,
+               bytes.byte11,
+               bytes.byte12,
+               bytes.byte13,
+               bytes.byte14,
+               bytes.byte15
+            } };
+         return uuid{ std::begin(arrbytes), std::end(arrbytes) };
+#else
+         return Uuid{};
+#endif
+    }
+}
---
+++ src/Engine/Utils/Uuid.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include "Engine/Core/shellpch.h"
+
+namespace Shell {
+
+    class Uuid {
+    public:
+        using value_type = uint8_t;
+
+        static Uuid Create();
+
+        Uuid(value_type(&arr)[16]) noexcept
+        {
+            std::copy(std::cbegin(arr), std::cend(arr), std::begin(m_Data));
+        }
+
+        constexpr Uuid(std::array<value_type, 16> const & arr) noexcept : m_Data{arr} {}
+
+        template<typename ForwardIterator>
+        explicit Uuid(ForwardIterator first, ForwardIterator last)
+        {
+            if (std::distance(first, last) == 16)
+                std::copy(first, last, std::begin(m_Data));
+        }
+
+        template<class CharT = char,
+                class Traits = std::char_traits<CharT>,
+                class Allocator = std::allocator<CharT>>
+        inline std::basic_string<CharT, Traits, Allocator> toString(Uuid const & id)
+        {
+            std::basic_stringstream<CharT, Traits, Allocator> sstr;
+            sstr << id;
+            return sstr.str();
+        }
+
+    private:
+        std::array<value_type, 16> m_Data{ { 0 } };
+
+        template <class Elem, class Traits>
+        friend std::basic_ostream<Elem, Traits> & operator<<(std::basic_ostream<Elem, Traits> &s, Uuid const & id);
+    };
+    template <class Elem, class Traits>
+    std::basic_ostream<Elem, Traits> & operator<<(std::basic_ostream<Elem, Traits> &s, Uuid const & id)
+    {
+        // save current flags
+        std::ios_base::fmtflags f(s.flags());
+
+        // manipulate stream as needed
+        s << std::hex << std::setfill(static_cast<Elem>('0'))
+          << std::setw(2) << (int)id.m_Data[0]
+          << std::setw(2) << (int)id.m_Data[1]
+          << std::setw(2) << (int)id.m_Data[2]
+          << std::setw(2) << (int)id.m_Data[3]
+          << '-'
+          << std::setw(2) << (int)id.m_Data[4]
+          << std::setw(2) << (int)id.m_Data[5]
+          << '-'
+          << std::setw(2) << (int)id.m_Data[6]
+          << std::setw(2) << (int)id.m_Data[7]
+          << '-'
+          << std::setw(2) << (int)id.m_Data[8]
+          << std::setw(2) << (int)id.m_Data[9]
+          << '-'
+          << std::setw(2) << (int)id.m_Data[10]
+          << std::setw(2) << (int)id.m_Data[11]
+          << std::setw(2) << (int)id.m_Data[12]
+          << std::setw(2) << (int)id.m_Data[13]
+          << std::setw(2) << (int)id.m_Data[14]
+          << std::setw(2) << (int)id.m_Data[15];
+
+        // restore original flags
+        s.flags(f);
+
+        return s;
+    }
+
+    template<class CharT = char,
+            class Traits = std::char_traits<CharT>,
+            class Allocator = std::allocator<CharT>>
+    inline std::basic_string<CharT, Traits, Allocator> UuidToString(Uuid const & id)
+    {
+        std::basic_stringstream<CharT, Traits, Allocator> sstr;
+        sstr << id;
+        return sstr.str();
+    }
+}
